0823069e92803c4bdac81646e27ae38a8735716d
Benjamin Bandla Khonye <bkhonye@gmail.com>
2022-10-18 07:36:10 +0200

    adding ui component files

---
+++ Documents/GitHub/bebakho/bebakho-project/components/Layout.tsx
@@ -0,0 +1,19 @@
+
+import Footer from './Footer';
+import Navbar from './Navbar'
+
+interface Props{
+    children: any;
+}
+
+const Layout = ({ children }: Props) => {
+    return (
+        <>
+            <Navbar />
+            {children}
+            <Footer />
+        </>
+    );
+};
+
+export default Layout
---
+++ Documents/GitHub/bebakho/bebakho-project/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+
+const Home: NextPage = () => {
+  return (
+    <div >
+     
+      <Head>
+        <title>bebakho farms </title>
+        <meta name="description" content="The one stop farm" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+       homepage
+      
+    </div>
+  )
+}
+
+export default Home
